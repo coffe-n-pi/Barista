81e2b2e119e7bf671d6907911f0d78952ef7a4d4
Linus Berg <linus@oc6478442246.ibm.com>
2019-04-26 10:33:44 +0200

    Ignore Model file.

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from cnn.yolo import YOLO
+from PIL import Image
+import numpy as np
+import cv2
+import json
+app = Flask(__name__)
+yolo = YOLO()
+
+def detect_img(img):
+  image = Image.fromarray(img)
+  detections = yolo.detect_image(image)
+  ret_det = dict()
+  for key in detections:
+    ret_det[yolo.GetClassFromIndex(key)] = int(detections[key])
+  print(ret_det)
+  return ret_det
+
+@app.route('/api/test', methods=['POST'])
+def img_recog():
+  print("Requesting obj detection")
+  r = request
+  # convert string of image data to uint8
+  nparr = np.fromstring(r.data, np.uint8)
+  # decode image
+  img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+  return json.dumps(detect_img(img))
+
+#yolo.close_session()
+#detect_img()
