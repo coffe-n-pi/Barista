5ff3f980a6287f2056816a19216f1bed8d2db966
Linus <linus@fenix.me.uk>
2019-05-13 13:12:34 +0200

    Stream.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, Response, request
 from cnn.yolo import YOLO
 from PIL import Image
 import numpy as np
 import cv2
 import json
 app = Flask(__name__)
-yolo = YOLO()
+#yolo = YOLO()
+LATEST_IMG = None
 
 def detect_img(img):
   image = Image.fromarray(img)
@@ -20,12 +21,27 @@ def detect_img(img):
 def reach():
   return "CAN REACH!"
 
+def gen():
+  global LATEST_IMG
+  """Video streaming generator function."""
+  while True:
+    frame = LATEST_IMG
+    yield (b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+  """Video streaming route. Put this in the src attribute of an img tag."""
+  return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
 @app.route('/api/analyse', methods=['POST'])
 def img_recog():
+  global LATEST_IMG
   print("Requesting obj detection")
   r = request
   # convert string of image data to uint8
   nparr = np.fromstring(r.data, np.uint8)
   # decode image
   img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-  return json.dumps(detect_img(img))
+  LATEST_IMG = r.data
+  return "{}" #json.dumps(detect_img(img))
